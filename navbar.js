d462e1230eda784af2d6dad98c001960db6336fb
HernandoVargasL <122489123+hernandovargaslopez@users.noreply.github.com>
2024-07-17 08:19:34 -0500

    ajustes primeros

---
+++ navbar.js
@@ -0,0 +1,43 @@
+
+const dropDownButon = document.querySelector(".nav-bar-toggle-igac");
+const nav = document.querySelector(".navbarigac");
+const navLogos = document.querySelector(".navbarigac .logos");
+const dropDownMenu = document.querySelector(".navbarnavigac");
+const dropDowntoggle = document.querySelector(".dropdownToggle");
+const dropDown = document.querySelector(".dropDown");
+const linklist = document.querySelector("#link-list")
+const logoIgac = document.querySelector(".navbar-brand-igac")
+const barraIgac = document.querySelector(".barra_gov")
+
+
+dropDownButon.addEventListener('click', function handleClick(event) {
+    dropDownMenu.classList.toggle('expandMenu');
+});
+
+// Tablet responsive
+function tabletSize(x) {
+    if (x.matches) {
+        dropDownMenu.append(linklist);
+    } else {
+        linklist.remove()
+        nav.append(linklist);
+    }
+}  
+var x = window.matchMedia("(max-width: 768px)")
+tabletSize(x)
+x.addListener(tabletSize)
+
+
+// Mobile responsive
+function tabletMobile(x) {
+    if (x.matches) {
+        barraIgac.append(logoIgac);
+    } else {
+        logoIgac.remove()
+        navLogos.append(logoIgac);
+    }
+  }
+  
+var x = window.matchMedia("(max-width: 526px)")
+tabletMobile(x)
+x.addListener(tabletMobile)
